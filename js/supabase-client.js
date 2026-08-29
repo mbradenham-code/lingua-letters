5136772bebf0cfa62819ed6cb4be8be168a845d9
mbradenham-code <malley.bradenham@gmail.com>
2026-08-29 19:18:03 -0400

    Add Supabase client initialization

---
+++ js/supabase-client.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+export const supabase = createClient(
+  'https://vfohvovtxklsdvpigycx.supabase.co',
+  'sb_publishable_iOcoEodjREKpEL1B7DqRSg_k4cKFSEu'
+)
